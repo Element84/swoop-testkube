6b46a57262bb3110b95b7a17e881a43eed7b0d4c
Hector Machin <hector@element84.com>
2023-09-06 09:49:33 -0400

    Removing console log

--- tests/e2e/step-1-swoop-api-get-processes.js
+++ tests/e2e/step-1-swoop-api-get-processes.js
@@ -3,9 +3,7 @@ import {check} from 'k6';
 
 
 export default function() {
-  console.log('Executing HTTP GET http://' + __ENV.API_HOST + '/processes/');
   const swoopApiProcesses = http.get('http://' + __ENV.API_HOST + '/processes/');
-  console.log('Response of HTTP GET http://' + __ENV.API_HOST + '/processes/: ', swoopApiProcesses.json());
   check(swoopApiProcesses, {
     'status of SWOOP API GET /processes was 200': (r) => r.status == 200
   });
