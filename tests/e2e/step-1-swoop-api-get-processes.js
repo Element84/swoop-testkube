1c78b5d2e4e564733d59e4a8161eb3372099d8bb
Hector Machin <hector@element84.com>
2023-09-05 21:54:32 -0400

    Adding e2e test process

---
+++ tests/e2e/step-1-swoop-api-get-processes.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import {check} from 'k6';
+
+
+export default function() {
+  const swoopApiProcesses = http.get('http://' + __ENV.API_HOST + '/processes/');
+  check(swoopApiProcesses, {
+    'status of SWOOP API GET /processes was 200': (r) => r.status == 200
+  });
+  check(swoopApiProcesses, {
+    'response of SWOOP API GET /processes contains a list of processes': (r) => r.json().processes == Array
+  });
+  check(swoopApiProcesses, {
+    'response of SWOOP API GET /processes contains mirror workflow': (r) => r.json().processes.filter(p => p.id == "mirror").length > 0
+  });
+}
