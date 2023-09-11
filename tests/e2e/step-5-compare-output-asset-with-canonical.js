eddefa15a799aa7e6c3743f943c7d16887d62b7d
Hector Machin <hector@element84.com>
2023-09-11 14:17:42 -0400

    Adding e2e test 5 comparing output asset with caninonical copy

--- tests/e2e/step-5-compare-output-asset-with-canonical.js
+++ tests/e2e/step-5-compare-output-asset-with-canonical.js
@@ -11,9 +11,7 @@ export const options = {
 };
 
 const awsConfig = new AWSConfig({
-  region: __ENV.AWS_REGION,
-  accessKeyId: __ENV.AWS_ACCESS_KEY_ID,
-  secretAccessKey: __ENV.AWS_SECRET_ACCESS_KEY,
+  region: __ENV.AWS_REGION
 });
 
 const s3 = new S3Client(awsConfig);
