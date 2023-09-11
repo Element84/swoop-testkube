a7ca235d0cc45b03362eed3841d972dac917501c
Hector Machin <hector@element84.com>
2023-09-11 11:54:13 -0400

    Adding e2e test 5 comparing output asset with caninonical copy

---
+++ tests/e2e/step-5-compare-output-asset-with-canonical.js
@@ -0,0 +1,35 @@
+import {check} from 'k6';
+import { AWSConfig, S3Client } from 'https://jslib.k6.io/aws/0.9.0/s3.js';
+
+export const options = {
+  stages: [
+    {
+      maxDuration: `${__ENV.TESTTIMEOUT}`,
+      target: parseInt(__ENV.TESTUSERTARGET)
+    },
+  ],
+};
+
+const awsConfig = new AWSConfig({
+  region: __ENV.AWS_REGION,
+  accessKeyId: __ENV.AWS_ACCESS_KEY_ID,
+  secretAccessKey: __ENV.AWS_SECRET_ACCESS_KEY,
+});
+
+const s3 = new S3Client(awsConfig);
+
+export default async function() {
+  const assetObjects = await s3.listObjects(__ENV.STAC_ASSET_BUCKET_NAME, `data/naip/${__ENV.TESTID}/`);
+
+  check(assetObjects, {
+    'output asset object exists on s3': (r) => objects.filter((r) => r.key === __ENV.STAC_ASSET_OBJECT_NAME).length > 0
+  });
+  
+  check(assetObjects, {
+    'output asset size matches canonical object': (r) => __ENV.STAC_ASSET_CANONICAL_SIZE == objects.filter((r) => r.key === __ENV.STAC_ASSET_OBJECT_NAME)[0].size
+  });
+
+  check(assetObjects, {
+    'output asset checksum matches canonical object': (r) => __ENV.STAC_ASSET_CANONICAL_CHECKSUM == objects.filter((r) => r.key === __ENV.STAC_ASSET_OBJECT_NAME)[0].etag
+  });
+}
